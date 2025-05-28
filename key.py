b13c5ea1116f2b3fddded46b0c7ca72010e6d3f5
bhuwanshrestha1 <shresthakritik@gmail.com>
2025-05-28 17:20:25 +0545

    Final commit of Major Project

--- key.py
+++
@@ -1,7 +0,0 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-import os
-
-# Generate a 256-bit AES key (32 bytes)
-key = os.urandom(32)  # For AES-256
-print(f"Generated AES Key: {key.hex()}")
